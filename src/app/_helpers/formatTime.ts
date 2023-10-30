9d64707edfed6c4a616c3a624bfab76948feb831
ochotnya <ochotny.a@gmail.com>
2023-10-30 12:12:51 +0100

    fix hydration error

--- src/app/_helpers/formatTime.ts
+++ src/app/_helpers/formatTime.ts
@@ -1,7 +1,7 @@
 import { DateTime } from 'luxon'
 
 export const formatDate = (dateString: Date): string | undefined => {
-  const date = DateTime.fromJSDate(dateString)
+  const date = DateTime.fromJSDate(dateString, { zone: 'pl' })
 
   return date.toFormat('dd-MM-yyyy HH:mm')
 }
